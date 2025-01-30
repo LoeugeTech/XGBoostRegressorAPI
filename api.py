87e3f120c10f87fa795269ba2c490a4957872fe4
LoeugeTech <wcloeuge@gmail.com>
2025-01-30 09:14:13 +0400

    Updated the API with entities

--- api.py
+++ api.py
@@ -20,7 +20,7 @@ class InputData(BaseModel):
     southeast: int
     southwest: int
 
-# Define Prediction endpoint
+# Define Prediction endpoints
 @app.post("/api/predict")
 def predict(data: InputData):
     input_features = [
@@ -37,7 +37,37 @@ def predict(data: InputData):
         "predicted_charges": predicted_charges
     }
 
+# Endpoint for sex values
+@app.get("/api/entities/sex")
+def get_sex_labels():
+    return {
+        "sex": [
+            {"value": 1, "label": "Male"},
+            {"value": 0, "label": "Female"}
+        ]
+    }
 
+# Endpoint for smoker values
+@app.get("/api/entities/smoker")
+def get_smoker_labels():
+    return {
+        "smoker": [
+            {"value": 1, "label": "Yes"},
+            {"value": 0, "label": "No"}
+        ]
+    }
+
+# Endpoint for region values
+@app.get("/api/entities/region")
+def get_region_labels():
+    return {
+        "region": [
+            {"columns": [1, 0, 0], "label": "Northwest"},
+            {"columns": [0, 1, 0], "label": "Southeast"},
+            {"columns": [0, 0, 1], "label": "Southwest"},
+            {"columns": [0, 0, 0], "label": "Northeast"}
+        ]
+    }
 # Run with: uvicorn app: app --reload
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8001)
